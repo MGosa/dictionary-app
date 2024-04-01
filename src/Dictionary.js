de6d1ebf9c768f8635efe86e034655ee82974b06
MGosa <149839103+MGosa@users.noreply.github.com>
2024-04-01 23:02:55 +0100

    added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,22 @@
 import { useState } from "react";
+import axios from "axios";
 
 
 export default function Dictionary(){
     let[keyword, setKeyword]=useState("")
 
+    function handleRespone(response){
+        console.log(response.data);
+
+    }
+
     function search(event){
         event.preventDefault();
-        alert (`Searching for ${keyword}`)
+      
+
+       let apiKey = "off12bea02cd1ft1f4f0c632ee3ca6b4";
+       let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+       axios.get(apiUrl).then(handleRespone);
     }
 
     function handleWordChange(event){
