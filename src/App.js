a51d5b6a55861e42c3c95df78b6f251261e8c48f
MGosa <149839103+MGosa@users.noreply.github.com>
2024-04-04 00:30:51 +0100

    updated footer

--- src/App.js
+++ src/App.js
@@ -13,8 +13,12 @@ function App() {
         </header>
       </div>
       <footer>
-        This project was coded by Monika Gosa and is open-sourced on{" "}
-        <a href="https://github.com/MGosa/dictionary-app" target="_blank">
+        This project was coded by Monika Gosa and is open-sourced on
+        <a
+          href="https://github.com/MGosa/dictionary-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           GitHub
         </a>
       </footer>
