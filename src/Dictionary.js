9cfe355d9a75182b3c77c637c129eea747a7815b
MGosa <149839103+MGosa@users.noreply.github.com>
2024-04-10 22:41:50 +0100

    Added images

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,23 @@
 import Results from "./Results";
 import { useState } from "react";
 import axios from "axios";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
  export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [result, setResult]=useState(null);
+  let[photos, setPhotos]= useState(null);
 
   function handleRespone(response) {
     setResult(response.data);
   }
+
+  function handleSheCodesResponse(response){
+    
+    setPhotos(response.data.photos);
+  }
+
 
   function search(event) {
     event.preventDefault();
@@ -18,6 +26,9 @@ import "./Dictionary.css";
     let apiKey = "off12bea02cd1ft1f4f0c632ee3ca6b4";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleRespone);
+
+    let SheCodesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+    axios.get(SheCodesApiUrl).then(handleSheCodesResponse);
   }
 
   function handleWordChange(event) {
@@ -31,6 +42,7 @@ import "./Dictionary.css";
         </form>
       </section>
       <Results result={result} />
+      <Photos photos={photos} />
     </div>
   );
 }
